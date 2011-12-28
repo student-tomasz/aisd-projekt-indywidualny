794c2908657a891f020920f34f9f9bc6f5fdb39c
Tomasz Cudziło <tomasz@luzid.co>
2011-12-28 09:39:38 +0100

    add simple debugging mode

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source :rubygems
 
 group :development do
   gem 'perftools.rb', :git => 'git://github.com/bearded/perftools.rb.git', :branch => 'perftools-1.8'
+  gem 'pry', '~> 0.9.7.4'
 end
---
+++ lib/application/debugging.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
+
+require 'rubygems'
+require 'bundler/setup'
+require 'pry'
+
+require 'array'
+require 'mesh'
+
+mesh = Mesh::Model.new ARGV[0]
+
+binding.pry
